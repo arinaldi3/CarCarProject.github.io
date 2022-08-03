0434cc06bafbed6b1737c7f64f844a8de45ad067
Nate Endicott <Nateendicott28@gmail.com>
2022-08-03 18:26:41 -0400

    Updated Service History List

--- ghi/app/src/Services/ServiceHistoryList.js
+++ ghi/app/src/Services/ServiceHistoryList.js
@@ -5,4 +5,13 @@ function ServiceAppointmentHistory() {
     const [search, setSearch] = useState ('')
     const [appointment, setAppointment] = useState ([])
     const [filteredAppointments, setfilteredAppointments] = useState ([])
+
+    const fetchAppointments = async () => {
+        const appointmentUrl = 'http://localhost:8080/api/appointments/'
+        const response = await fetch(appointmentUrl)
+        const appointmentData = await response.json();
+        setAppointment(appointmentData.appointments)
+    }
+
+    const handleClick = async
 }
